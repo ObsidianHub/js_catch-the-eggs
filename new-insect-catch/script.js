4ab2ba6a6e18f8091916f7343e7309829105d893
Denny Pearce <obsidian.y.dragon@gmail.com>
2021-02-09 23:10:52 +0200

    declare base variables, add listeners for button start

--- new-insect-catch/script.js
+++ new-insect-catch/script.js
@@ -1,1 +1,15 @@
-const screens = document.querySelectorAll(".screen");
+const screens = document.querySelectorAll(".screen"),
+  choose_insect_btns = document.querySelectorAll(".choose-insect-btn"),
+  start_btn = document.getElementById("start-btn"),
+  game_container = document.getElementById("game-container"),
+  timeEl = document.getElementById("time"),
+  scoreEl = document.getElementById("score"),
+  message = document.getElementById("message");
+
+let seconds = 0,
+  score = 0,
+  selected_insect = {};
+
+start_btn.addEventListener("click", () => {
+  screens[0].classList.add("up");
+});
